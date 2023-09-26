39f3532f9bab37fe752b3a3ada30077bba1c4eac
Rajesh Kalyanam <39563013+rkalyanapurdue@users.noreply.github.com>
2023-09-25 22:37:43 -0500

    Update create_processor_yml.py

--- create_processor_yml.py
+++ create_processor_yml.py
@@ -6,7 +6,7 @@ import os
 deminput_jobid = os.environ['param_input_path']
 input_path = f'/compute_scratch/{deminput_jobid}'
 
-processor_dict = {'$1': {'DEMMerge': {'input_path':os.environ['param_input_path'],'merged_filename':os.environ['param_merged_filename']}}}
+processor_dict = {'$1': {'DEMMerge': {'input_path':input_path,'merged_filename':os.environ['param_merged_filename']}}}
 
 with open('/job/executable/demmerge.yml','w') as demfile:
     yaml.dump(processor_dict,demfile)
